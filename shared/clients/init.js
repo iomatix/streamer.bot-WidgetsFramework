8921ecdf92ae129d0d7c7ff1d18156ae107de07f
iomatix <mateuszwypchlak@gmail.com>
2026-01-16 22:21:11 +0100

    Feature flags

--- shared/clients/init.js
+++ shared/clients/init.js
@@ -2,6 +2,11 @@ import { createTikfinityClient } from "./tikfinity-client.js";
 import { createStreamlootsClient } from "./streamloots-client.js";
 
 export function initCustomClients(onEvent) {
-    createTikfinityClient((event, data) => onEvent(event, data));
-    createStreamlootsClient((event, data) => onEvent(event, data));
+    if (window.ENABLE_TIKTOK) {
+        createTikfinityClient((event, data) => onEvent(event, data));
+    }
+
+    if (window.ENABLE_STREAMLOOTS) {
+        createStreamlootsClient((event, data) => onEvent(event, data));
+    }
 }
