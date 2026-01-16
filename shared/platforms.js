b40e65552284df1949ab8372fd6e00d35b63423b
iomatix <mateuszwypchlak@gmail.com>
2026-01-16 22:04:33 +0100

    Platforms.js updated

--- shared/platforms.js
+++ shared/platforms.js
@@ -1,11 +1,33 @@
 export const PLATFORM_ICONS = {
-    twitch: "/widgets/nameplate/icons/twitch.png",
-    youtube: "/widgets/nameplate/icons/youtube.png",
-    kick: "/widgets/nameplate/icons/kick.png"
+    twitch: "/assets/icons/platforms/twitch.png",
+    youtube: "/assets/icons/platforms/youtube.png",
+    kick: "/assets/icons/platforms/kick.png",
+    kofi: "/assets/icons/platforms/kofi.png",
+    patreon: "/assets/icons/platforms/patreon.png",
+    streamelements: "/assets/icons/platforms/streamelements.png",
+    streamlabs: "/assets/icons/platforms/streamlabs.png",
+    tipeee: "/assets/icons/platforms/tipeee.png",
+    fourthwall: "/assets/icons/platforms/fourthwall.png",
+    crowdcontrol: "/assets/icons/platforms/crowdcontrol.png",
+    donordrive: "/assets/icons/platforms/donordrive.png",
+    treatstream: "/assets/icons/platforms/treatstream.png",
+    tiktok: "/assets/icons/platforms/tiktok.png",
+    streamloots: "/assets/icons/platforms/streamloots.png"
 };
 
 export const PLATFORM_COLORS = {
     twitch: "#9146FF",
     youtube: "#FF0000",
-    kick: "#53fc18"
+    kick: "#53FC18",
+    kofi: "#13C3FF",
+    patreon: "#FF424D",
+    streamelements: "#00A8FF",
+    streamlabs: "#00FFB3",
+    tipeee: "#FF3366",
+    fourthwall: "#6C5CE7",
+    crowdcontrol: "#FFB400",
+    donordrive: "#0077CC",
+    treatstream: "#FF7A00",
+    tiktok: "#FE2C55",
+    streamloots: "#6A00FF"
 };
