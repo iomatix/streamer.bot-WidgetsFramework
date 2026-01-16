1129a6a91b6e5140e6f107dbc41b56422d6e7334
iomatix <mateuszwypchlak@gmail.com>
2026-01-16 22:10:16 +0100

    Fourthwall file name fix

--- shared/events/fourthwall/index.js
+++ shared/events/fourthwall/index.js
@@ -2,5 +2,5 @@ export { FourthwallOrderPlacedAdapter } from "./order.js";
 export { FourthwallDonationAdapter } from "./donation.js";
 export { FourthwallSubscriptionPurchasedAdapter } from "./subscription.js";
 export { FourthwallGiftPurchaseAdapter } from "./giftpurchase.js";
-export { FourthwallGiftDrawStartedAdapter } from "./giftdrawstarted.js";
-export { FourthwallGiftDrawEndedAdapter } from "./giftdrawended.js";
+export { FourthwallGiftDrawStartedAdapter } from "./giftdrawstart.js";
+export { FourthwallGiftDrawEndedAdapter } from "./giftdrawend.js";
