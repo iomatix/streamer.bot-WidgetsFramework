a7c9ca5b7326dd06bd836663cbae575656d3a8bf
iomatix <mateuszwypchlak@gmail.com>
2026-01-17 15:30:08 +0100

    Chat notification fix

--- shared/events/twitch/chatmessage.js
+++ shared/events/twitch/chatmessage.js
@@ -1,16 +1,24 @@
 import { GetAvatar } from "../../utils/helpers.js";
 
 export async function TwitchChatMessageAdapter(data) {
+    const username =
+        data.message?.displayName ||
+        data.message?.username ||
+        data.user?.name ||
+        data.user?.login ||
+        "UnknownUser";
+
+
     return {
         platform: "twitch",
         type: "chat",
         subtype: "",
         tags: ["twitch", "chat"],
-        username: data.displayName,
+        username,
         description: "",
         attribute: "",
-        message: data.message,
-        avatar: await GetAvatar(data.userName, "twitch"),
+        message: data.message.message, // tekst wiadomości
+        avatar: await GetAvatar(username, "twitch"),
         raw: data
     };
 }
--- shared/events/twitch/firstwords.js
+++ shared/events/twitch/firstwords.js
@@ -1,16 +1,23 @@
 import { GetAvatar } from "../../utils/helpers.js";
 
 export async function TwitchFirstWordsAdapter(data) {
+    const username =
+        data.message?.displayName ||
+        data.message?.username ||
+        data.user?.name ||
+        data.user?.login ||
+        "UnknownUser";
+
     return {
         platform: "twitch",
         type: "firstwords",
         subtype: "",
         tags: ["twitch", "chat", "firstwords"],
-        username: data.displayName,
+        username,
         description: "sent their first message!",
         attribute: "",
-        message: data.message,
-        avatar: await GetAvatar(data.userName, "twitch"),
+        message: data.message.message,
+        avatar: await GetAvatar(username, "twitch"),
         raw: data
     };
 }
